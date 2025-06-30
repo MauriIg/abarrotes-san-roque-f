398f443338ff913c8c4b94ba845658c90478108b
Mauricio Damian Gomez Galdamez <maugogal18@gmail.com>
2025-06-30 17:07:51 -0600

    Fix: adjusted userSlice to parse backend login response

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
 
         {/* Rutas protegidas para admin */}
         <Route path="/dashboard" element={
-          <PrivateRoute allowedRoles={["admin"]}>
+          <PrivateRoute allowedRoles={["admin","cajero"]}>
             <Dashboard />
           </PrivateRoute>
         } />
