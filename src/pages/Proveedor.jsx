f636c9ff86086773707b7b92b1e70b2da6d29978
Mauricio Damian Gomez Galdamez <L21270287@tuxtla.tecnm.mx>
2025-07-01 19:35:35 -0600

    “comentario”

--- src/pages/Proveedor.jsx
+++ src/pages/Proveedor.jsx
@@ -89,7 +89,7 @@ const Proveedor = () => {
               <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
                 <img src={logo} alt="Logo" style={{ height: "80px" }} />
                 <div>
-                  <strong>Usuario Cajero:</strong> {usuario.nombre || usuario.email}
+                  <strong>Usuario Proveedor:</strong> {usuario.nombre || usuario.email}
                 </div>
               </div>
       
